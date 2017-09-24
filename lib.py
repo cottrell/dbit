7362de9a617e7327a5a716ed5d5d580ed6a7e345
David Cottrell <cottrell@users.noreply.github.com>
2017-09-24 15:26:14 +0100

    Fix file path

--- lib.py
+++ lib.py
@@ -2,7 +2,7 @@ from functools import lru_cache
 import json
 import deribit_api as da
 import os
-cred = json.load(open(os.path.abspath('~/.cred/deribit/deribit.json')))
+cred = json.load(open(os.path.expanduser('~/.cred/deribit/deribit.json')))
 # url = 'https://test.deribit.com'
 url = 'https://www.deribit.com'
 client = da.RestClient(cred['access_key'], cred['access_secret'], url=url)
