70510440ac25f21518793cd1378c908c9a027c87
David Cottrell <cottrell@users.noreply.github.com>
2017-10-06 21:16:06 +0100

    update

--- setup.sh
+++ setup.sh
@@ -2,8 +2,8 @@
 conda env list | grep -s airflow || conda create --name airflow
 source activate airflow
 # https://issues.apache.org/jira/browse/AIRFLOW-1102
-conda install pandas
-pip install airflow
+conda install -y pandas
+pip install -y airflow
 pip uninstall -y gunicorn
-pip install gunicorn==19.3.0
+pip install -y gunicorn==19.3.0
 # airflow initdb
