b7adb0416e7b607f8822f19aa6f486d981650d84
David Cottrell <cottrell@users.noreply.github.com>
2017-10-12 00:00:41 +0100

    scipy

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 conda env list | grep -s airflow || conda create --name airflow
 source activate airflow
 # https://issues.apache.org/jira/browse/AIRFLOW-1102
-conda install -y pandas
+conda install -y pandas scipy
 pip install airflow git+git://github.com/deribit/deribit-api-python.git
 pip uninstall -y gunicorn
 pip install gunicorn==19.3.0
