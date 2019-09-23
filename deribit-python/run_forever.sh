bf66551e24c70fc9068e900706ed871aa9bfc7ef
David Cottrell <cottrell@users.noreply.github.com>
2019-09-23 07:59:21 +0100

    update

--- deribit-python/run_forever.sh
+++ deribit-python/run_forever.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env sh
 DIR="$( cd "$(dirname "$0")" ; pwd -P )"
 while true; do
-    $DIR/timeout.sh 300 $DIR/do.py
+    # $DIR/timeout.sh 300 $DIR/do.py
+    $DIR/do.py
     sleep 1
 done
